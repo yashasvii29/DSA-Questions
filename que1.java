e9013785cc5483bf032bac749407ff8729a7f36d
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2023-03-19 03:45:53 +0530

    Add files via upload

--- que1.java
+++ que1.java
@@ -1,35 +1,16 @@
 public class que1 {
-    public static void main(String[] args)
-     {
-        //Scanner sc=new Scanner(System.in);
-        int n1=1  ;
-        int n2=1000;
-        for(int i=1;i<=1000;i++){
-                IsArmstrongnum(i);
-        }
-
+    public static void main(String[] args){
+        int n=4;
+        PrintPath(0,n,"");
     }
-    public static void IsArmstrongnum(int i)
-    {
-        int d=countofdigit(i);
-        int ans=0;
-        int temp=i;
-        while(i>0){
-            int rem=i%10;
-            ans=(int)(ans+Math.pow(rem,d));
-            i=i/10;
+    public static void PrintPath(int src,int des,String ans){
+        if(src==des){
+            System.out.println(ans+" ");
+            return;
         }
-        if(ans==temp)
-            System.out.println(temp);
-        
-        
+        if(src>des)
+            return ;
+        PrintPath(src+1,des,ans+1);
+        PrintPath
     }
-    public static int countofdigit(int i){
-        int c=0;
-        while(i>0){
-            i/=10;
-            c++;
-        }
-        return c;
-    }   
 }
