e4b80448da89fc25041dd2702adbd93b51c8d450
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2023-04-14 15:42:10 +0530

    Add files via upload

---
+++ DynamicStack.java
@@ -0,0 +1,12 @@
+public class DynamicStack extends Stack {
+    public void push(int item) throws Exception{
+        if(super.isfull()){
+            int arr[]=new int[2*super.data.length];
+            for(int i=0;i<super.data.length;i++){
+                arr[i]=super.data[i];
+            }
+            super.data=arr;
+        }
+        super.push(item);
+    }
+}
