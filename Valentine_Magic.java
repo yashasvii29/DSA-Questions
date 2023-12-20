5550542baa8bc19b1ded80038da17df9a14fce02
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2023-12-20 23:12:11 +0530

    Add files via upload

---
+++ Valentine_Magic.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Valentine_Magic {
+    public static void main(String[] args) {
+        int boys[]={2,11,3};
+        int girls[]={5,7,3,2};
+        Arrays.sort(boys);
+        Arrays.sort(girls);
+        System.out.println(MinDiff(boys, girls, 0, 0));
+    }
+    public static int MinDiff(int boys[],int girls[],int i,int j){
+        if(i==boys.length){
+            return 0;
+        }
+        if(j==girls.length){
+            return 9999999;
+        }
+
+        int pair=Math.abs(boys[i]-girls[j])+MinDiff(boys,girls,i+1,j+1);
+        int No_Pair=MinDiff(boys, girls, i, j+1);
+        return Math.min(pair,No_Pair);
+    }
+}
