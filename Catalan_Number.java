166e63a157669a72fac02ae17cb5939b960bd071
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2024-05-03 21:13:47 +0530

    Add files via upload

---
+++ Catalan_Number.java
@@ -0,0 +1,16 @@
+public class Catalan_Number {
+    public static void main(String[] args) {
+        int n=4;
+        System.out.println(Catalan(n));
+    }
+    public static int Catalan(int n){
+        if(n==0){
+            return 1;
+        }
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans=ans+Catalan(i-1)*Catalan(n-i);
+        }
+        return ans;
+    }
+}
